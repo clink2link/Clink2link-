5c3f515eec11053374e015adaa542a08dd99d1f3
clink2link <clink2link@gmail.com>
2026-07-26 20:01:16 +0700

    Update buy.js

--- js/buy.js
+++ js/buy.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded",async()=>{
 
+const buyBox=document.getElementById("buyBox");
 const code=location.pathname.split("/").pop();
 
 if(!code){
-document.getElementById("buyBox").innerHTML="<h3>Link tidak valid</h3>";
+buyBox.innerHTML="<h3>Link tidak valid</h3>";
 return;
 }
 
@@ -13,28 +14,34 @@ try{
 
 const link=await database.getLinkByCode(code);
 
-if(!link||link.link_type!=="sell"){
-document.getElementById("buyBox").innerHTML="<h3>Link tidak ditemukan</h3>";
+console.log("LINK:",link);
+
+if(!link){
+buyBox.innerHTML="<h3>Link tidak ditemukan</h3>";
 return;
 }
 
-document.getElementById("buyBox").innerHTML=`
+if(link.link_type!=="sell"){
+buyBox.innerHTML="<h3>Link ini bukan Sell Link</h3>";
+return;
+}
+
+buyBox.innerHTML=`
 <div class="sell-card">
 <h2>${link.title}</h2>
-<p>Harga: <b>Rp ${Number(link.price).toLocaleString("id-ID")}</b></p>
+<p>Harga: <b>Rp ${Number(link.price||0).toLocaleString("id-ID")}</b></p>
 <button class="btn-sell" id="payBtn">Bayar Sekarang</button>
 </div>
 `;
 
 document.getElementById("payBtn").onclick=()=>{
-console.log("Bayar:",link);
+console.log("PAY:",link);
 };
 
 }catch(err){
 
-console.error("BUY ERROR:",err);
-
-document.getElementById("buyBox").innerHTML="<h3>Terjadi kesalahan</h3>";
+console.error(err);
+buyBox.innerHTML="<h3>Terjadi kesalahan</h3>";
 
 }
 
