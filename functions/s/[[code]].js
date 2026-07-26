38a84c88bb147066645378df77a1d222057ade66
clink2link <clink2link@gmail.com>
2026-07-26 20:50:25 +0700

    Update [[code]].js

--- functions/s/[[code]].js
+++ functions/s/[[code]].js
@@ -3,15 +3,13 @@ export async function onRequest(context){
 const code=context.params.code;
 
 if(!code){
-
 return new Response("Link tidak ditemukan.",{
 status:404
 });
-
 }
 
 return Response.redirect(
-`/s/index.html?code=${encodeURIComponent(code)}`,
+`/task1.html?code=${encodeURIComponent(code)}`,
 302
 );
 
