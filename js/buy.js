cf80e87960d32880a3672ce25f6ad02f6a1b3abe
clink2link <clink2link@gmail.com>
2026-07-29 16:10:33 +0700

    Update buy.js

--- js/buy.js
+++ js/buy.js
@@ -5,8 +5,8 @@ const buyBox=document.getElementById("buyBox");
 const code=window.BUY_CODE||location.pathname.split("/").pop();
 
 if(!code||code==="b"||code==="buy"){
-buyBox.innerHTML="<h3>Link tidak valid</h3>";
-return;
+    buyBox.innerHTML="<h3>Link tidak valid</h3>";
+    return;
 }
 
 console.log("BUY CODE:",code);
@@ -18,29 +18,77 @@ const link=await database.getLinkByCode(code);
 console.log("LINK:",link);
 
 if(!link){
-buyBox.innerHTML="<h3>Link tidak ditemukan</h3>";
-return;
+    buyBox.innerHTML="<h3>Link tidak ditemukan</h3>";
+    return;
 }
 
-if(link.link_type!=="sell"){
-buyBox.innerHTML="<h3>Link ini bukan Sell Link</h3>";
-return;
+if(link.link_type!=="sell"&&link.type!=="sell"){
+    buyBox.innerHTML="<h3>Link ini bukan Sell Link</h3>";
+    return;
 }
 
 buyBox.innerHTML=`
 <div class="sell-card">
-<h2>${link.title}</h2>
-<p>Harga: <b>Rp ${Number(link.price||0).toLocaleString("id-ID")}</b></p>
-<button class="btn-sell" id="payBtn">Bayar Sekarang</button>
+<h2>${link.title||"Sell Link"}</h2>
+
+<p>
+Harga:
+<b>Rp ${Number(link.price||0).toLocaleString("id-ID")}</b>
+</p>
+
+<p>
+Terjual:
+<b>${link.sold||0}x</b>
+</p>
+
+<button class="btn-sell" id="payBtn">
+<i class="fa-solid fa-cart-shopping"></i>
+Bayar Sekarang
+</button>
+
 </div>
 `;
 
-document.getElementById("payBtn").addEventListener("click",()=>{
+document.getElementById("payBtn").addEventListener("click",async()=>{
 
 console.log("PAY:",link);
 
-// TODO:
-// Panggil BayarGG / payment.html di sini
+try{
+
+const payment=calculateSellPayment(link.price);
+
+const order=await database.createSellOrder({
+
+link_id:link.id,
+
+seller_id:link.user_id,
+
+buyer_id:null,
+
+price:Number(link.price),
+
+fee:payment.fee,
+
+seller_receive:payment.seller_receive,
+
+destination_url:link.destination_url||link.destination
+
+});
+
+console.log("ORDER CREATED:",order);
+
+alert("Order berhasil dibuat. Lanjut pembayaran.");
+
+// NANTI BAYARGG:
+// location.href="../payment.html?id="+order.id;
+
+}catch(err){
+
+console.error("CREATE ORDER ERROR:",err);
+
+alert("Gagal membuat order.");
+
+}
 
 });
 
