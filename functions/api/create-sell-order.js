eca2424c43309f4cacaa8e1ddb4511a639447dd7
clink2link <clink2link@gmail.com>
2026-08-03 19:49:23 +0700

    Update create-sell-order.js

--- functions/api/create-sell-order.js
+++ functions/api/create-sell-order.js
@@ -1,39 +1,13 @@
-export async function onRequestPost(context){
-
-const {request}=context;
-
-try{
-
-const body=await request.json();
+export async function onRequestPost(){
 
 return new Response(
-JSON.stringify({
-success:true,
-message:"POST FUNCTION ACTIVE",
-body
-}),
+"INI CREATE SELL ORDER BARU",
 {
+status:200,
 headers:{
-"Content-Type":"application/json"
+"Content-Type":"text/plain"
 }
 }
 );
-
-}catch(error){
-
-return new Response(
-JSON.stringify({
-success:false,
-error:error.message
-}),
-{
-status:500,
-headers:{
-"Content-Type":"application/json"
-}
-}
-);
-
-}
 
 }
