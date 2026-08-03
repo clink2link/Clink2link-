fd14d3174044391c42c097a363c6f17691e9d1de
clink2link <clink2link@gmail.com>
2026-08-03 15:51:12 +0700

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,29 @@
+<?php
+
+// =======================================
+// CLICK2PAY CONFIG
+// =======================================
+
+date_default_timezone_set("Asia/Jakarta");
+
+define(
+    "SUPABASE_URL",
+    "https://lwjtagxkqeprjpupmadf.supabase.co"
+);
+
+// GANTI DENGAN SERVICE ROLE KEY
+define(
+    "SUPABASE_SERVICE_KEY",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3anRhZ3hrcWVwcmpwdXBtYWRmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDMwMTE3NiwiZXhwIjoyMDk5ODc3MTc2fQ.jaiA6dZ2IWMh2gJuS9qPEorNlXpMT5BgyXaRRSJCSvk"
+);
+
+// BayarGG
+define(
+    "BAYARGG_API_KEY",
+    "API-69804014a5f32c5394b3b7f67e1cf6f622b9f214e10edb2a"
+);
+
+define(
+    "MARKET_FEE",
+    20
+);
