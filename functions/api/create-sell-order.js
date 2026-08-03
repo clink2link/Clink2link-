3ad298c131684a6b4c1e7cbaf78646c506606cef
clink2link <clink2link@gmail.com>
2026-08-03 19:57:11 +0700

    Update create-sell-order.js

--- functions/api/create-sell-order.js
+++ functions/api/create-sell-order.js
@@ -1,13 +1,80 @@
-export async function onRequestPost(){
+export async function onRequestPost(context){
+
+const {request,env}=context;
+
+try{
+
+console.log("CREATE SELL ORDER START");
+
+
+let body;
+
+try{
+
+body=await request.json();
+
+console.log("BODY:",body);
+
+}catch(e){
+
+throw new Error(
+"JSON BODY ERROR: "+e.message
+);
+
+}
+
 
 return new Response(
-"INI CREATE SELL ORDER BARU",
+JSON.stringify({
+
+success:true,
+
+message:"CREATE SELL ORDER FUNCTION OK",
+
+body:body,
+
+env:{
+supabase:!!env.SUPABASE_URL,
+key:!!env.SUPABASE_SERVICE_KEY,
+fee:env.MARKET_FEE||null
+}
+
+}),
 {
 status:200,
 headers:{
-"Content-Type":"text/plain"
+"Content-Type":"application/json"
 }
 }
 );
+
+
+}catch(error){
+
+
+console.log(
+"FUNCTION ERROR:",
+error
+);
+
+
+return new Response(
+JSON.stringify({
+
+success:false,
+
+error:error.message
+
+}),
+{
+status:500,
+headers:{
+"Content-Type":"application/json"
+}
+}
+);
+
+
+}
 
 }
