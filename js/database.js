61c33a8d5dd25d8fbf1b0da048878a27750777bc
clink2link <clink2link@gmail.com>
2026-08-11 04:42:03 +0700

    Update database.js

--- js/database.js
+++ js/database.js
@@ -7,7 +7,7 @@
 const SUPABASE_URL =
     "https://lwjtagxkqeprjpupmadf.supabase.co";
 const SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJsd2p0YWd4a3FlcHJqcHVwbWFkZiIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg0MzAxMTc2LCJleHAiOjIwOTk4NzcxNzZ9.Cg8TIBtOE4PHmnSybJtMqEoCFx-Qm4Kkl8exSOanTes";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3anRhZ3hrcWVwcmpwdXBtYWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMDExNzYsImV4cCI6MjA5OTg3NzE3Nn0.Cg8TIBtOE4PHmnSybJtMqEoCFx-Qm4Kkl8exSOanTes";
 const API_URL =
     "https://click2pay.my.id";
 // =====================================================
